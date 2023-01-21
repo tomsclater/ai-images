2b4da3b858f135e737e5dd334366e5a5c144ed13
Thomas Sclater <tomsclater@live.com>
2023-01-21 15:47:01 -0800

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Loader, Card, FormField } from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
-    <div>Home</div>
+    <section className="max-w-7xl">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>  
+      </div>      
+    </section>
   )
 }
 
