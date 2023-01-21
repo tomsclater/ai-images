b0fdb1f165cdbb7dbeefe7dc97fbeccb6078fd55
Thomas Sclater <tomsclater@live.com>
2023-01-21 15:57:36 -0800

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,11 +5,33 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
+  const [searchText, setSearchText] = useState('') 
+
   return (
     <section className="max-w-7xl">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>  
-      </div>      
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Browse through our range of AI art</p>
+      </div>   
+      <div className="mt-16">
+        <FormField />
+      </div>   
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+          </>
+        )}
+      </div>   
     </section>
   )
 }
