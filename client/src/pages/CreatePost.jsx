cc6db94e2e58fc1e58f656a5bba73b5f14cb8792
Thomas Sclater <tomsclater@live.com>
2023-01-21 19:08:20 -0800

    Update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -93,6 +93,16 @@ const CreatePost = () => {
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
+
+        <div className="mt-10">
+              <p className="mt-2 text-[#666e75] text-[14px]">Share your images with the world:</p>
+              <button 
+                type="submit"
+                className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              >
+                {loading ? 'Sharing...' : 'Share'}
+              </button>
+        </div>
       </form>
     </section>
   )
