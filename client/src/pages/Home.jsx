9eff30ff785ef8fc61643d93cc131577ce0bc865
Thomas Sclater <tomsclater@live.com>
2023-01-21 16:57:38 -0800

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from 'react';
+
 import { Loader, Card, FormField } from '../components';
+
+const RenderCards = ( {data, title} ) => {
+  if(data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />)
+  } 
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+      {title}
+    </h2>
+  )
+}
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -29,6 +42,19 @@ const Home = () => {
                 Showing results for <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards
+                  data={[]}
+                  title="No search results found"
+                />
+              ) : (
+                <RenderCards 
+                  data={[]}
+                  title="No posts found"
+                />
+              )}
+            </div>
           </>
         )}
       </div>   
