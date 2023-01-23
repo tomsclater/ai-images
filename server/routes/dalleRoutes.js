cdbdeef768d19fb334571af221eef4357932dac9
Thomas Sclater <tomsclater@live.com>
2023-01-22 17:11:41 -0800

    Images generating

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,7 +16,7 @@ router.route('/').get((req, res) => {
     res.send('Hello from the AI');
 });
 
-router.router('/').post(async (req, res) => {
+router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
@@ -28,8 +28,11 @@ router.router('/').post(async (req, res) => {
         });
 
         const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({ photo: image });
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
     }
 })
 
