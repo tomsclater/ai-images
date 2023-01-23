0574d337468b8db9961c2fd70745f17732f0d141
Thomas Sclater <tomsclater@live.com>
2023-01-23 01:06:07 -0800

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,8 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -47,6 +49,17 @@ const Home = () => {
     fetchPosts();
   }, [])
   
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
 
   return (
     <section className="max-w-7xl">
@@ -55,7 +68,14 @@ const Home = () => {
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Browse through our range of AI art</p>
       </div>   
       <div className="mt-16">
-        <FormField />
+        <FormField 
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>   
 
       <div className="mt-10">
@@ -73,7 +93,7 @@ const Home = () => {
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
                 <RenderCards
-                  data={[]}
+                  data={searchedResults}
                   title="No search results found"
                 />
               ) : (
