fd8c197073e349beedb7d0e94d85e569bae9b309
Thomas Sclater <tomsclater@live.com>
2023-01-23 00:45:06 -0800

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,35 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setSearchText] = useState('') 
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if(response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, [])
+  
 
   return (
     <section className="max-w-7xl">
@@ -50,7 +78,7 @@ const Home = () => {
                 />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
